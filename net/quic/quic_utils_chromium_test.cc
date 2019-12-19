024348c36d8d36eed1432ba1768dd0343b24cb1f
Victor Vasiliev <vasilvv@chromium.org>
2019-12-19 00:38:08 +0000

    Remove unused code from //net/quic/quic_utils_chromium.h
    R=rch@chromium.org
    
    Change-Id: Ib9e27b96a4f2b7bb6975444dc3ba57472afa6897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1974611
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Victor Vasiliev <vasilvv@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726176}

--- net/quic/quic_utils_chromium_test.cc
+++ net/quic/quic_utils_chromium_test.cc
@@ -26,37 +26,6 @@ TEST(QuicUtilsChromiumTest, ParseQuicConnectionOptions) {
   EXPECT_EQ(expected_options, parsed_options);
 }
 
-TEST(QuicUtilsChromiumTest, FindOrNullTest) {
-  std::map<int, int> m;
-  m[0] = 2;
-
-  // Check FindOrNull
-  int* p1 = FindOrNull(m, 0);
-  CHECK_EQ(*p1, 2);
-  ++(*p1);
-  const std::map<int, int>& const_m = m;
-  const int* p2 = FindOrNull(const_m, 0);
-  CHECK_EQ(*p2, 3);
-  CHECK(FindOrNull(m, 1) == nullptr);
-}
-
-TEST(QuicUtilsChromiumTest, FindOrDieTest) {
-  std::map<int, int> m;
-  m[10] = 15;
-  EXPECT_EQ(15, FindOrDie(m, 10));
-  // TODO(rtenneti): Use the latest DEATH macros after merging with latest rch's
-  // changes.
-  // ASSERT_DEATH(FindOrDie(m, 8), "Map key not found: 8");
-
-  // Make sure the non-const reference returning version works.
-  FindOrDie(m, 10) = 20;
-  EXPECT_EQ(20, FindOrDie(m, 10));
-
-  // Make sure we can lookup values in a const map.
-  const std::map<int, int>& const_m = m;
-  EXPECT_EQ(20, FindOrDie(const_m, 10));
-}
-
 }  // namespace
 }  // namespace test
 }  // namespace net
