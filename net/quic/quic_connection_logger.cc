ec69e5e20088d95f1e197b7ec3c6e22a5cb49c36
Ryan Hamilton <rch@chromium.org>
2019-12-20 02:52:15 +0000

    Add new histograms for recording the size of sent QUIC packets.
    Change-Id: I72e1cacf310884720870043d263045181e1d3744
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1975995
    Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#726664}

--- net/quic/quic_connection_logger.cc
+++ net/quic/quic_connection_logger.cc
@@ -30,6 +30,7 @@
 #include "net/third_party/quiche/src/quic/core/quic_time.h"
 #include "net/third_party/quiche/src/quic/core/quic_utils.h"
 
+using quic::kMaxOutgoingPacketSize;
 using std::string;
 
 namespace net {
@@ -596,6 +597,44 @@ void QuicConnectionLogger::OnPacketSent(
     const quic::SerializedPacket& serialized_packet,
     quic::TransmissionType transmission_type,
     quic::QuicTime sent_time) {
+  // 4.4.1.4.  Minimum Packet Size
+  // The payload of a UDP datagram carrying the Initial packet MUST be
+  // expanded to at least 1200 octets
+  const quic::QuicPacketLength kMinClientInitialPacketLength = 1200;
+  const quic::QuicPacketLength encrypted_length =
+      serialized_packet.encrypted_length;
+  switch (serialized_packet.encryption_level) {
+    case quic::ENCRYPTION_INITIAL:
+      UMA_HISTOGRAM_CUSTOM_COUNTS("Net.QuicSession.SendPacketSize.Initial",
+                                  encrypted_length, 1, kMaxOutgoingPacketSize,
+                                  50);
+      if (encrypted_length < kMinClientInitialPacketLength) {
+        UMA_HISTOGRAM_CUSTOM_COUNTS(
+            "Net.QuicSession.TooSmallInitialSentPacket",
+            kMinClientInitialPacketLength - encrypted_length, 1,
+            kMinClientInitialPacketLength, 50);
+      }
+      break;
+    case quic::ENCRYPTION_HANDSHAKE:
+      UMA_HISTOGRAM_CUSTOM_COUNTS("Net.QuicSession.SendPacketSize.Hanshake",
+                                  encrypted_length, 1, kMaxOutgoingPacketSize,
+                                  50);
+      break;
+    case quic::ENCRYPTION_ZERO_RTT:
+      UMA_HISTOGRAM_CUSTOM_COUNTS("Net.QuicSession.SendPacketSize.0RTT",
+                                  encrypted_length, 1, kMaxOutgoingPacketSize,
+                                  50);
+      break;
+    case quic::ENCRYPTION_FORWARD_SECURE:
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          "Net.QuicSession.SendPacketSize.ForwardSecure", encrypted_length, 1,
+          kMaxOutgoingPacketSize, 50);
+      break;
+    case quic::NUM_ENCRYPTION_LEVELS:
+      NOTREACHED();
+      break;
+  }
+
   if (!net_log_.IsCapturing())
     return;
   net_log_.AddEvent(NetLogEventType::QUIC_SESSION_PACKET_SENT, [&] {
